8f5a83a1a769615cafbc0d9aca7cfa860d397271
Annsh Agrawaal <agrawaalannsh@gmail.com>
2022-03-31 15:43:27 -0500

    Updated queries.sql

--- sql/queries.sql
+++ sql/queries.sql
@@ -4,7 +4,7 @@ use uic_covid_database;
 
 select first_dose, second_dose
 from vaccination_card
-where Name = "Annsh Agrawaal";
+where Name = "Vedant";
 
 select *
 from student_info
@@ -12,7 +12,7 @@ where rec_status = 1
 order by UIN;
 
 select *
-from student_info
+from vaccination_card
 where vaccine_status > 0;
 
 -- Aggregrate Queries
@@ -26,16 +26,17 @@ select *
 from vaccination_card
 where name_of_vaccine like '%Pfizer%';
 
-select first_dose, date(booster_dose) as booster_dose_date
-from vaccination_card;
-
+select UIN, first_dose, date(second_dose) as second_dose_date
+from vaccination_card
+group by second_dose_date;
 
 -- Complex Queries
 
 select a.vaccine_status, b.first_dose, b.second_dose, b.booster_dose
 from student_info a
 left join vaccination_card b
-on a.UIN = b.UIN;
+on a.UIN = b.UIN
+where b.booster_dose >= 0;
 
 select *
 from student_info
